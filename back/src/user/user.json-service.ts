2eea13fac4512ca61d8ff7c9496faf3b4e2b4f14
tashiga <sasisashtiga2002@gmail.com>
2023-11-02 11:40:28 +0100

    init and update back

---
+++ back/src/user/user.json-service.ts
@@ -0,0 +1,138 @@
+import { User } from './user';
+import { UserService } from './user.service';
+import * as fs from 'fs';
+// import { v4 as uuidv4 } from 'uuid';
+
+export class UserJSONService implements UserService {
+
+    add(username: string): User {
+        if(username!== null || username !== undefined){
+            //need to check if username already exist
+            // if(this.getByUsername(username)==null){
+                try {
+                    let user:User = {
+                        username: username,
+                        id: 4
+                    };
+                    const data = fs.readFileSync('data.json', 'utf-8');
+                    const users: User[] = JSON.parse(data);
+                    //need to check if user already exists
+                    users.push(user);
+                    const jsonString = JSON.stringify(users);
+                    fs.writeFileSync('data.json', jsonString, 'utf-8');
+                    console.log('Fichier JSON créé avec succès.');
+                  } catch (error) {
+                    console.error('Erreur lors de la création du fichier JSON :', error);
+                  }
+            // }
+            // else {
+            //     console.log("username already exists !");'
+            // }
+            //add new user in data file
+            
+        }
+        return {username: username, id:4};
+        // throw new Error('Method not implemented.');
+    }
+
+    getById(id: number): User | null {
+        let user:User|undefined;
+        try {
+            const data = fs.readFileSync('data.json', 'utf-8');
+            const users: User[] = JSON.parse(data);
+            user = users.find(user=> user.id === id);
+            console.log('user finded :', user);
+        }
+        catch(error){
+            console.error('Erreur lors de la conversion du JSON :', error);
+        }
+        
+        if(user !== undefined) 
+            return user;
+        else 
+            return null;
+        // throw new Error('2.Method not implemented.');
+    }
+
+    // getUsers(): User[] {
+    //     let users: User[] = [];
+    //     try {
+    //         const data = fs.readFileSync('data.json', 'utf-8');
+    //         const allUsers : User[] = JSON.parse(data);
+    //         console.log('2.Contenu du fichier JSON :', users);
+    //         users = allUsers;
+    //     }
+    //     catch(error){
+    //         console.error('Erreur lors de la conversion du JSON :', error);
+    //     }
+    //     console.log("users: ", users);
+    //     return users;
+
+    // }
+
+    getByUsername(username: string): User | null {
+        let user:User|undefined;
+        try {
+            const data = fs.readFileSync('data.json', 'utf-8');
+            const users: User[] = JSON.parse(data);
+            console.log('Contenu du fichier JSON :', users.length, "\nuser : ", users.find(user=> user.username === username));
+            user = users.find(user=> user.username === username);
+        }
+        catch(error){
+            console.error('Erreur lors de la conversion du JSON :', error);
+        }
+        
+        if(user !== undefined) 
+            return user;
+        else 
+            return null;
+        // throw new Error('2.Method not implemented.');
+    }
+
+    deleteUser(id: number): void{
+        let userIndex:number;
+        try {
+            const data = fs.readFileSync('data.json', 'utf-8');
+            const users: User[] = JSON.parse(data);
+            userIndex = users.findIndex(user=> user.id === id)
+            console.log('user finded at :', userIndex);
+            if(userIndex===0) 
+                users.shift();
+            else 
+                users.splice(userIndex, userIndex);
+            const jsonString = JSON.stringify(users);
+            fs.writeFileSync('data.json', jsonString, 'utf-8');
+            console.log('User deleted from Fichier JSON.');
+        }
+        catch(error){
+            console.error('Erreur lors de la conversion du JSON :', error);
+        }
+        
+        // throw new Error('3.Method not implemented.');
+    }
+
+    updateById(id: number, username: string): User{
+        throw new Error('4.Method not implemented.');
+    }
+
+    getAllUsers(): User[]{
+        let usersToReturn: User[] = [];
+        try {
+            const data = fs.readFileSync('data.json', 'utf-8');
+            const users: User[] = JSON.parse(data);
+            console.log('Contenu du fichier JSON :', users.length,);
+            for (let index = 0; index < users.length; index++) {
+                usersToReturn.push(users[index]);
+            }
+            console.log("finally users : ", usersToReturn);
+        }
+        catch(e) {
+            console.log("error : ", e);
+        }
+        finally {
+            return usersToReturn;
+        }
+       
+    }
+      
+}
---
+++ back/src/user/user.router.ts
@@ -0,0 +1,59 @@
+import { Router } from 'express';
+import { UserController } from './user.controller';
+
+export class UserRouter {
+    router = Router();
+
+    constructor(private userController: UserController) {
+        this.configureRoutes();
+    }
+
+    private configureRoutes(): void {
+        this.router.get('/user/:id', (req, res, next) => {
+            console.log("[User] - try to get (", req.params.id, ")");
+            try {
+                const result = this.userController.getById(
+                    parseInt(req.params.id),
+                );
+                res.status(200).json(result);
+            } catch (error: unknown) {
+                next(error);
+            }
+            console.log("-----------------------------");
+        });
+
+        this.router.post('/add-user', (req, res, next) => {
+            console.log("[Add-user] - try to get...");
+            try {
+                const result = this.userController.add(req.body.username);
+                res.status(200).json(result);
+            } catch (error: unknown) {
+                next(error);
+            }
+            console.log("-----------------------------");
+        });
+
+        this.router.get('/all-users/',(req, res, next) => {
+            console.log("[All-users] - try to get...");
+            try {
+                const result = this.userController.getAllUsers();
+                res.status(200).json(result);
+            } catch (error: unknown) {
+                next(error);
+            } 
+        console.log("-----------------------------");});
+
+        this.router.delete('/delete/:id',(req, res, next) => {
+            console.log("[Delete] - try to delete (", req.params.id, ")");
+            try {
+                const result = this.userController.deleteUser(
+                    parseInt(req.params.id),
+                );
+                res.status(200).json(result);
+            } catch (error: unknown) {
+                next(error);
+            }
+            console.log("-----------------------------");
+        });
+    }
+}
