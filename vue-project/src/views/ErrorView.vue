61a1d2a92d3dfa55fc5f890ae62ed4d847abaf4b
tashiga <sasisashtiga2002@gmail.com>
2024-02-09 00:44:28 +0100

    Add error page if bad url

---
+++ vue-project/src/views/ErrorView.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <div class="errorMessage">
+            <h1>{{ $t('errorMessage')}}</h1>
+        </div>
+    </main>
+</template>
+
+<style>
+.errorMessage{
+    color : white;
+}
+</style>
